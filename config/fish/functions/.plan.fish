dba4529bff27623a685226962410bf19206ca8ae
Pete Neisen <pete@neisen.xyz>
2016-12-13 08:48:18 -0700

    Finally got the .plan function working

--- config/fish/functions/.plan.fish
+++ config/fish/functions/.plan.fish
@@ -1,18 +1,24 @@
 function .plan
+  echo "Looking for .plan."
   if not test -e ~/.plan
+    echo "Plan not found. Cloning..."
     git clone git@github.com:pneisen/.plan.git ~/.plan
   end
 
   pushd ~/.plan
 
+  echo "Updating .plan from Github."
   git pull
 
   vim README.md
 
-  if test -n (git diff --shortstat 2> /dev/null | tail -n1)
+  if test (git diff --shortstat 2> /dev/null | tail -n1)
+    echo "Changes made to .plan. Committing..."
     set -l date (date)
     git commit -am "$date update"
     git push
+  else
+    echo "No changes made to .plan."
   end
 
   popd
